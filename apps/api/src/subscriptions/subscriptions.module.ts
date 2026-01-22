2bfd348a4db91cb8b68d67bdd66e9dcab562ade3
Timur <your_email@example.com>
2026-01-23 00:16:00 +0500

    add

--- apps/api/src/subscriptions/subscriptions.module.ts
+++ apps/api/src/subscriptions/subscriptions.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { SubscriptionsController } from './subscriptions.controller';
 import { SubscriptionsService } from './subscriptions.service';
 import { AppleReceiptService } from './apple-receipt.service';
+import { GeoModule } from '../geo/geo.module';
 
 @Module({
+    imports: [GeoModule],
     controllers: [SubscriptionsController],
     providers: [SubscriptionsService, AppleReceiptService],
     exports: [SubscriptionsService, AppleReceiptService],
