305d934ebc03a2729669e171b9f0ea9d9d3fecda
Timur <your_email@example.com>
2026-05-25 02:32:10 +0500

    updated version

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -4,7 +4,7 @@ export default {
     name: "EatSense",
     slug: "eatsense",
     owner: "eatsense",
-    version: "2.0.48",
+    version: "2.0.49",
     orientation: "default",
     // EAS Update configuration
     updates: {
@@ -37,7 +37,7 @@ export default {
 
     ios: {
       bundleIdentifier: "ch.eatsense.app",
-      buildNumber: "66",
+      buildNumber: "67",
       developmentTeam: "73T7PB4F99",
       supportsTablet: false,
       infoPlist: {
@@ -74,7 +74,7 @@ export default {
 
     android: {
       package: "ch.eatsense.app",
-      versionCode: 103,
+      versionCode: 104,
       adaptiveIcon: { foregroundImage: "./assets/logo/Logo.jpg", backgroundColor: "#FFFFFF" },
       permissions: ["CAMERA", "RECORD_AUDIO", "MODIFY_AUDIO_SETTINGS", "READ_MEDIA_IMAGES", "ACCESS_FINE_LOCATION", "ACCESS_COARSE_LOCATION", "POST_NOTIFICATIONS"],
       intentFilters: [{
